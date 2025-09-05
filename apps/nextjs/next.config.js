fbb5092b551af33dbadd12a2f27feefc99a7a8c1
manato kuroda <a7108100@gmail.com>
2025-09-05 16:30:44 +0900

    refactor: add external modules to webpack config
    Added `utf-8-validate` and `bufferutil` as external modules in the Next.js webpack configuration. This ensures proper handling of optional dependencies and improves build compatibility.

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -7,6 +7,10 @@ module.exports = {
     if (nextRuntime === 'nodejs') {
       config.resolve.alias.canvas = false;
     }
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+    });
     return config;
   },
 };
